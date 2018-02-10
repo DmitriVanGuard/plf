70b9496796514622da67cc2050e9b0b7555648b8
DmitriVanGuard <dimavanguard@gmail.com>
2018-02-10 12:16:09 +0200

    package.json setup

---
+++ route/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+	context: __dirname,
+	entry: ['./js/ClientApp.jsx'],
+	devtool: 'cheap-eval-source-map',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js',
+		publicPath: '/public/'
+	},
+	resolve: {
+		extensions: ['.js']
+	},
+	stats: {
+		colors: true,
+		reasons: true,
+		chunks: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/, // enything that ends with JS or JSX runs through babel
+				include: [path.resolve('js')]
+			}
+		]
+	}
+};
+
+if (process.env.NODE_ENV === 'production') {
+	config.entry = './js/app.js';
+	config.devtool = false; // no source maps
+	config.plugins = [];
+}
+
+module.exports = config;
