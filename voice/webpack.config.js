fc33002f41c432f0c89ed4178da163ad11771f65
DmitriVanGuard <dimavanguard@gmail.com>
2018-02-10 15:35:48 +0200

    WebRTC Voice Char project setup

---
+++ voice/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+	context: __dirname,
+	entry: ['./js/app.js'],
+	devtool: 'cheap-eval-source-map',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js',
+		publicPath: '/public/'
+	},
+	resolve: {
+		extensions: ['.js']
+	},
+	stats: {
+		colors: true,
+		reasons: true,
+		chunks: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js/, // enything that ends with JS or JSX runs through babel
+				include: [path.resolve('js')]
+			}
+		]
+	}
+};
+
+if (process.env.NODE_ENV === 'production') {
+	config.entry = './js/app.js';
+	config.devtool = false; // no source maps
+	config.plugins = [];
+}
+
+module.exports = config;
